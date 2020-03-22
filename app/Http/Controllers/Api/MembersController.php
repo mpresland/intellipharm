9199af3b7eaab6726a2a99966434d270c231cd72
Matt Presland <matt.p@travelonline.com>
2020-03-23 08:50:44 +1000

    DB - Tidy up models

--- app/Http/Controllers/Api/MembersController.php
+++ app/Http/Controllers/Api/MembersController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\MembersRequest;
 use App\Http\Resources\Members;
-use App\Member;
+use App\Models\Member;
 
 class MembersController extends Controller
 {
