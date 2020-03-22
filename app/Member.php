ce95702d9071dd94075ac04b5325f3c65683dde6
Matt Presland <matt.p@travelonline.com>
2020-03-23 08:37:48 +1000

    DB - Add Member model

---
+++ app/Member.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    /**
+     * In a more complex application Member DB settings and relationships would be defined here
+     */
+}
