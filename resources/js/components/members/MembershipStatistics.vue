ca8ef76f0fca24439a1b3f187df0cd34dd537d66
Matt Presland <matt.p@travelonline.com>
2020-03-23 11:51:31 +1000

    UI - MemberStatistics, handle year drill down

--- resources/js/components/members/MembershipStatistics.vue
+++ resources/js/components/members/MembershipStatistics.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="root-element">
-    <h3>New Memberships By Year</h3>
+    <h3>{{ label }}</h3>
+    <h4 v-if="mode == 'year'">Click on a year to view detailed data</h4>
+    <button
+      type="button"
+      class="btn btn-secondary"
+      v-if="mode == 'month'"
+      v-on:click="resetChart"
+    >Back</button>
     <membership-statistics-graph
       v-if="loaded"
       :chartdata="chartData"
@@ -17,6 +24,8 @@ export default {
   components: { MembershipStatisticsGraph },
   data: function() {
     return {
+      label: "New Members By Year",
+      mode: "year",
       loaded: false,
       chartData: {
         labels: [],
@@ -26,27 +35,47 @@ export default {
   },
   methods: {
     chartClicked(event) {
-      var selectedIndex = event[0]._index;
+      if (this.mode == "year") {
+        var selectedIndex = event[0]._index;
+        this.fetchStats(this.chartData.labels[selectedIndex]);
+        console.log("chart clicked");
+        console.log(this.chartData.labels[selectedIndex]);
+      }
+    },
+    fetchStats(year) {
+      this.loaded = false;
+      var url = "/api/membership-statistics";
 
-      console.log("chart clicked");
-      console.log(this.chartData.labels[selectedIndex]);
+      if (year) {
+        var url = url + "?year=" + year;
+        this.label = "New Members By Month For " + year;
+        this.mode = "month";
+      }
+
+      axios.get(url).then(response => {
+        this.chartData.labels = response.data.map(function(item) {
+          return item.period;
+        });
+        this.chartData.datasets = [
+          {
+            label: this.label,
+            backgroundColor: "#343a40",
+            data: response.data.map(function(item) {
+              return item.count;
+            })
+          }
+        ];
+        this.loaded = true;
+      });
+    },
+    resetChart() {
+      this.mode = "year";
+      this.label = "New Members By Year";
+      this.fetchStats();
     }
   },
-  async mounted() {
-    this.loaded = false;
-    axios.get("/api/membership-statistics").then(response => {
-      this.chartData.labels = response.data.map(function(item) {
-        return item.period;
-      });
-      this.chartData.datasets.push({
-        label: "New Members By Year",
-        backgroundColor: "#343a40",
-        data: response.data.map(function(item) {
-          return item.count;
-        })
-      });
-      this.loaded = true;
-    });
+  mounted() {
+    this.fetchStats();
   }
 };
 </script>
