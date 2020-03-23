b8bf3e26b82d2d9972a83d3bebd7bd7b6243a8be
Matt Presland <matt.p@travelonline.com>
2020-03-23 11:51:04 +1000

    API - MemberStatistics, handle year filter

--- app/Http/Controllers/Api/MemberStatisticsController.php
+++ app/Http/Controllers/Api/MemberStatisticsController.php
@@ -10,9 +10,16 @@ class MemberStatisticsController extends Controller
 {
     public function index(Request $request)
     {
-        $query = DB::table('members')
-            ->select(DB::raw("DATE_FORMAT(joined_date, '%Y') period"), DB::raw('count(id) as count'))
-            ->groupBy('period')
+        $query = DB::table('members');
+
+        if ($request->has('year') && !is_null($request->year)) {
+            $query->select(DB::raw("DATE_FORMAT(joined_date, '%m') period"), DB::raw('count(id) as count'))
+                ->whereYear('joined_date', $request->year);
+        } else {
+            $query->select(DB::raw("DATE_FORMAT(joined_date, '%Y') period"), DB::raw('count(id) as count'));
+        }
+
+        $query->groupBy('period')
             ->orderBy('period');
 
         return response($query->get());
