ca3efd45b87dfd6b313af6fa7cff5fb3b1a87ba2
Matt Presland <matt.p@travelonline.com>
2020-03-23 10:06:30 +1000

    UI - Add member filters

---
+++ resources/js/components/members/MembersFilters.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="root-element">
+    <p>
+      Find Members:
+      <input
+        type="text"
+        v-model="filters.firstname"
+        @input="updateFilters"
+        placeholder="First Name"
+      />
+      <input type="text" v-model="filters.surname" @input="updateFilters" placeholder="Surname" />
+      <input type="text" v-model="filters.email" @input="updateFilters" placeholder="Email" />
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      filters: {
+        firstname: null,
+        surname: null,
+        email: null
+      }
+    };
+  },
+  methods: {
+    updateFilters: function() {
+      this.$emit("update:filters", this.filters);
+    }
+  }
+};
+</script>
--- resources/js/components/members/MembersIndex.vue
+++ resources/js/components/members/MembersIndex.vue
@@ -1,31 +1,65 @@
 <template>
   <div class="root-element">
     <h1>Members Widget</h1>
+    <members-filters :filters.sync="filters"></members-filters>
     <members-table :members="members"></members-table>
   </div>
 </template>
 
 <script>
+import MembersFilters from "./MembersFilters.vue";
 import MembersTable from "./MembersTable.vue";
 
 export default {
   components: {
+    MembersFilters,
     MembersTable
   },
   data: function() {
     return {
-      members: []
+      members: [],
+      filters: {
+        firstname: null,
+        surname: null,
+        email: null
+      }
     };
+  },
+  computed: {
+    filterParams() {
+      const params = new URLSearchParams();
+
+      if (this.filters.firstname != null && this.filters.firstname.length) {
+        params.append("firstname", this.filters.firstname);
+      }
+      if (this.filters.surname != null && this.filters.surname.length) {
+        params.append("surname", this.filters.surname);
+      }
+      if (this.filters.email != null && this.filters.email.length) {
+        params.append("email", this.filters.email);
+      }
+      return params;
+    }
   },
   methods: {
     fetchMembers: function() {
       axios
-        .get("/api/members")
+        .get("/api/members", {
+          params: this.filterParams
+        })
         .then(response => (this.members = response.data.data));
     }
   },
   mounted() {
     this.fetchMembers();
+  },
+  watch: {
+    filters: {
+      handler: function() {
+        this.fetchMembers();
+      },
+      deep: true
+    }
   }
 };
 </script>
